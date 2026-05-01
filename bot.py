1e338020c8fad05d067c94a5acd10f0a534a49a9
grdgdfhgdgbrf <133863853+grdgdfhgdgbrf@users.noreply.github.com>
2026-05-01 21:11:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ import uuid
 import re
 
 # ==================== КОНФИГУРАЦИЯ ====================
-TOKEN = '8670879387:AAGz1v65wqhThDmwGNzCaEY9SY24XDJYLFE'
+TOKEN = '8670879387:AAH70T6P0ZEn-rvPhQo7rhrNMl9wUKDkILI'
 ADMIN_ID = 5356400377
 bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
 
